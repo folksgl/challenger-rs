3bfb348a59417aa68be14d3a5d39104d901bc4d2
Garrett <Gfolks14@gmail.com>
2020-12-28 22:02:08 -0500

    add tests for isready and setoption

--- src/uci.rs
+++ src/uci.rs
@@ -35,7 +35,7 @@ impl Command {
                 r"^debug (on|off)$",
                 r"^position (startpos|([rnbqkp12345678RNBQKP]{1,8}/){7}[rnbqkp12345678RNBQKP]{1,8} (w|b) (-|[KQkq]{1,4}) (-|[a-h][1-8]) (\\d)+ (\\d)+)( moves( [a-h][1-8][a-h][1-8][rnbqRNBQ]?)+)?$",
                 r"^go( ponder| infinite| (wtime|btime|winc|binc|movestogo|depth|nodes|mate|movetime) [\\d]+| searchmoves( [a-h][1-8][a-h][1-8][rnbqRNBQ]?)+)*$",
-                r"^setoption [\\w]+( value [\\w]+)?$"
+                r"^setoption [[:word:]]+( value [[:word:]]+)?$"
             ]).unwrap();
 
         if uci_regex_set.is_match(&input) {
@@ -123,23 +123,74 @@ mod tests {
     test_invalid_command!(invalid_debug_15, "debug\noff");
 
     // Valid isready
+    test_valid_command!(valid_isready_1, "isready", vec!["isready"]);
+
     // Invalid isready
+    test_invalid_command!(invalid_isready_1, "iisready");
+    test_invalid_command!(invalid_isready_2, "isreadyy");
+    test_invalid_command!(invalid_isready_3, "is ready");
+    test_invalid_command!(invalid_isready_4, "a isready");
+    test_invalid_command!(invalid_isready_5, "isready a");
+    test_invalid_command!(invalid_isready_6, "asdfisready");
+    test_invalid_command!(invalid_isready_7, "isreadyasdf");
+    test_invalid_command!(invalid_isready_8, "sready");
+    test_invalid_command!(invalid_isready_9, "i\nsready");
+    test_invalid_command!(invalid_isready_10, "i\tsready");
+    test_invalid_command!(invalid_isready_11, "isready isready");
+    test_invalid_command!(invalid_isready_12, "isready$");
+    test_invalid_command!(invalid_isready_13, "^isready");
+    test_invalid_command!(invalid_isready_14, "isready\nisready");
 
     // Valid setoption
+    test_valid_command!(
+        valid_setoption_1,
+        "setoption name value x",
+        vec!["setoption", "name", "value", "x"]
+    );
+    test_valid_command!(
+        valid_setoption_2,
+        "setoption name value 1",
+        vec!["setoption", "name", "value", "1"]
+    );
+    test_valid_command!(
+        valid_setoption_3,
+        "setoption asdf_1234",
+        vec!["setoption", "asdf_1234"]
+    );
+    test_valid_command!(
+        valid_setoption_4,
+        "setoption asdf_1234 value asdf_1234",
+        vec!["setoption", "asdf_1234", "value", "asdf_1234"]
+    );
+
     // Invalid setoption
+    test_invalid_command!(invalid_setoption_1, "isetoption");
+    test_invalid_command!(invalid_setoption_2, "setoptiony");
+    test_invalid_command!(invalid_setoption_3, "set option");
+    test_invalid_command!(invalid_setoption_4, "setoptionn name value x");
+    test_invalid_command!(invalid_setoption_5, "ssetoption name value x");
+    test_invalid_command!(invalid_setoption_6, "etoption asdf");
+    test_invalid_command!(invalid_setoption_7, "setoption value 42");
+    test_invalid_command!(invalid_setoption_8, "setoption 42 24");
+    test_invalid_command!(invalid_setoption_9, "setoption\n name value x");
 
     // Valid ucinewgame
+    //test_valid_command!(valid__1, "", vec![""]);
     // Invalid ucinewgame
 
     // Valid position
+    //test_valid_command!(valid__1, "", vec![""]);
     // Invalid position
 
     // Valid go
+    //test_valid_command!(valid__1, "", vec![""]);
     // Invalid go
 
     // Valid stop
+    //test_valid_command!(valid__1, "", vec![""]);
     // Invalid stop
 
     // Valid ponderhit
+    //test_valid_command!(valid__1, "", vec![""]);
     // Invalid ponderhit
 }
