471d8788db43b4fe4b9aec10cc475e7f10a575a6
folksgl <Gfolks14@gmail.com>
2021-02-27 12:00:37 -0500

    Improve variable names

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use std::sync::mpsc;
 use std::thread;
 
 fn main() {
-    let (tx, rx) = mpsc::channel();
+    let (sender, receiver) = mpsc::channel();
 
-    let producer_handle = thread::spawn(move || challenger_rs::producer(tx));
-    let consumer_handle = thread::spawn(move || challenger_rs::consumer(rx));
+    let producer_handle = thread::spawn(move || challenger_rs::producer(sender));
+    let consumer_handle = thread::spawn(move || challenger_rs::consumer(receiver));
 
     producer_handle.join().unwrap();
     consumer_handle.join().unwrap();
