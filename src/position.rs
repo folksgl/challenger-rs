37712d1f247c9b6f9c44566ba49c7a74fea28005
folksgl <Gfolks14@gmail.com>
2021-05-12 16:10:37 -0400

    Add module comment

--- src/position.rs
+++ src/position.rs
@@ -1,3 +1,8 @@
+// position.rs will primarily serve to expose the Position struct. A Position
+// represents a single chess position. It includes fields representing the
+// color to move, current piece positions, castling rights, etc. A Position
+// serves as a complete snapshot of a point in time of a chess match.
+
 pub struct Position {
     pieces: [u64; 14],
 }
