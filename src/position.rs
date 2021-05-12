3bfb8508b9ce5263d6e15c5ed33f32d33acb8602
folksgl <Gfolks14@gmail.com>
2021-05-12 16:13:18 -0400

    Add file constants

--- src/position.rs
+++ src/position.rs
@@ -3,6 +3,15 @@
 // color to move, current piece positions, castling rights, etc. A Position
 // serves as a complete snapshot of a point in time of a chess match.
 
+const A_FILE: u64 = 0x0101010101010101;
+const B_FILE: u64 = 0x0202020202020202;
+const C_FILE: u64 = 0x0404040404040404;
+const D_FILE: u64 = 0x0808080808080808;
+const E_FILE: u64 = 0x1010101010101010;
+const F_FILE: u64 = 0x2020202020202020;
+const G_FILE: u64 = 0x4040404040404040;
+const H_FILE: u64 = 0x8080808080808080;
+
 pub struct Position {
     pieces: [u64; 14],
 }
